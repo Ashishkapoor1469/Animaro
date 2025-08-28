bb6601ae6dd8f47ac95bbfb2b7109f8c1fd247d9
Ashish <kapoorashish713@gmail.com>
2025-08-28 22:06:33 +0530

    REMAKE

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,9 +2,8 @@ import type { Metadata } from "next";
 import "./globals.css";
 import Menu from "@/components/shared/menu/page";
 import localFont from "next/font/local";
-
 export const font1 = localFont({
-  src:"../../public/font/1.ttf"
+  src:"../font/1.ttf"
 })
 export const metadata: Metadata = {
   title: "Ashish protfolio",
